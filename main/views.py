a9f0643e0c2ae83ce68dcb9380fb2b6d1acbf7f9
Andri Qeleposhi <2576728q@student.gla.ac.uk>
2024-03-11 23:17:52 +0000

    Added some coolness after the submission

--- main/views.py
+++ main/views.py
@@ -85,7 +85,9 @@ def report_post(request, post_id):
         form = ReportForm(request.POST, instance=PostReport(reporter=request.user.userprofile, post_id=post))
         if form.is_valid():
             form.save()
-            return redirect('main:view_post', user_profile_slug=post.created_by.slug, post_slug=post.slug)
+            messages.success(request, 'Report submitted successfully!')
+            #return redirect('main:view_post', user_profile_slug=post.created_by.slug, post_slug=post.slug)
+            return render(request, 'photoGraph/report_post.html', {'post': post, 'form': form, 'show_popup': True})
     else:
         form = ReportForm()
     return render(request, 'photoGraph/report_post.html', {'post': post, 'form': form})
